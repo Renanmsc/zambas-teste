d9a03adafa9a41eaa2d40c29b760a3a3d4f191b6
rmcardoso <renanmello05@gmail.com>
2025-11-10 11:30:10 -0300

    teste

---
+++ src/main/java/insper/edu/br/PFZAMBAS/Filme/FilmeController.java
@@ -0,0 +1,36 @@
+package insper.edu.br.PFZAMBAS.Filme;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/filmes")
+public class FilmeController {
+
+    @Autowired
+    private FilmeService filmeService;
+
+    @GetMapping
+    public List<Filme> getFilmes() {
+        return filmeService.getFilmes();
+    }
+
+    @PostMapping
+    public Filme createFilme(@RequestBody Filme filme) {
+        return filmeService.createFilme(filme);
+    }
+
+    @GetMapping("/{id}")
+    public Filme getFilme(@PathVariable Integer id) {
+        return filmeService.getFilme(id);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public void deleteFilme(@PathVariable Integer id) {
+        filmeService.deleteFilme(id);
+    }
+}
---
+++ src/main/java/insper/edu/br/PFZAMBAS/Filme/FilmeRepository.java
@@ -0,0 +1,8 @@
+package insper.edu.br.PFZAMBAS.Filme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmeRepository extends JpaRepository<Filme, Integer> {
+}
